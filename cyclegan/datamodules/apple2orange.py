c410714712686f779e6d0de302231ecf728019d7
Jeong-ro Seok <jeongroseok@gmail.com>
2021-09-25 05:13:44 +0900

    first commit

---
+++ cyclegan/datamodules/apple2orange.py
@@ -0,0 +1,105 @@
+import os
+from typing import Optional, Union
+
+from pl_bolts.datamodules.vision_datamodule import VisionDataModule
+from pl_examples import _DATASETS_PATH
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms as transform_lib
+
+from ..datasets import ImageDataset, RandomZippingDataset
+
+
+class Apple2OrangeDataModule(VisionDataModule):
+    name = "apple2orange"
+    dims = (3, 256, 256)
+
+    def __init__(
+        self,
+        data_dir: Optional[str] = _DATASETS_PATH,
+        val_split: Union[int, float] = 0.2,
+        num_workers: int = 4,
+        batch_size: int = 32,
+        seed: int = 42,
+        shuffle: bool = False,
+        pin_memory: bool = False,
+        drop_last: bool = False,
+        persistent_workers: bool = True,
+        *args: any,
+        **kwargs: any,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.data_dir = data_dir if data_dir is not None else os.getcwd()
+        self.val_split = val_split
+        self.num_workers = num_workers
+        self.batch_size = batch_size
+        self.seed = seed
+        self.shuffle = shuffle
+        self.pin_memory = pin_memory
+        self.drop_last = drop_last
+        self.persistent_workers = persistent_workers
+
+    def prepare_data(self, *args: any, **kwargs: any) -> None:
+        pass
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        dataset_dir = os.path.join(self.data_dir, self.name)
+        if stage == "fit" or stage is None:
+            train_transforms = (
+                self.default_transforms()
+                if self.train_transforms is None
+                else self.train_transforms
+            )
+            val_transforms = (
+                self.default_transforms()
+                if self.val_transforms is None
+                else self.val_transforms
+            )
+
+            a_train = ImageDataset(
+                os.path.join(dataset_dir, "trainA"), train_transforms
+            )
+            b_train = ImageDataset(
+                os.path.join(dataset_dir, "trainB"), train_transforms
+            )
+            self.dataset_train = RandomZippingDataset([a_train, b_train], True)
+
+            a_val = ImageDataset(os.path.join(dataset_dir, "trainA"), val_transforms)
+            b_val = ImageDataset(os.path.join(dataset_dir, "trainB"), val_transforms)
+            self.dataset_val = RandomZippingDataset([a_val, b_val], True)
+
+            # Split
+            self.dataset_train = self._split_dataset(self.dataset_train)
+            self.dataset_val = self._split_dataset(self.dataset_val, train=False)
+
+        if stage == "test" or stage is None:
+            test_transforms = (
+                self.default_transforms()
+                if self.test_transforms is None
+                else self.test_transforms
+            )
+
+            a_test = ImageDataset(os.path.join(dataset_dir, "testA"), test_transforms)
+            b_test = ImageDataset(os.path.join(dataset_dir, "testB"), test_transforms)
+            self.dataset_test = RandomZippingDataset([a_test, b_test], True)
+
+    def default_transforms(self) -> callable:
+        return transform_lib.Compose(
+            [
+                transform_lib.ToTensor(),
+                transform_lib.Normalize(mean=(0.5,), std=(0.5,)),
+            ]
+        )
+
+    def _data_loader(self, dataset: Dataset, shuffle: bool = False) -> DataLoader:
+        return DataLoader(
+            dataset,
+            batch_size=self.batch_size,
+            shuffle=shuffle,
+            num_workers=self.num_workers,
+            drop_last=self.drop_last,
+            pin_memory=self.pin_memory,
+            persistent_workers=self.persistent_workers
+            if self.num_workers > 0
+            else False,
+        )
---
+++ cyclegan/datasets.py
@@ -0,0 +1,96 @@
+from genericpath import sameopenfile
+import os
+from typing import Any, Callable, List, Optional, Tuple, TypeVar, cast
+
+import torch
+from torch.utils.data import Dataset
+from torchvision.datasets.folder import (
+    IMG_EXTENSIONS,
+    default_loader,
+    has_file_allowed_extension,
+)
+
+T_co = TypeVar("T_co", covariant=True)
+T = TypeVar("T")
+
+
+class RandomZippingDataset(Dataset[Tuple[T_co]]):
+    def __init__(
+        self, datasets: List[Dataset[T_co]], random_sample: bool = True
+    ) -> None:
+        super().__init__()
+        self.datasets = datasets
+        self.random_sample = random_sample
+
+        self.dataset_len = len(self.datasets[0])
+
+    def __getitem__(self, index: int) -> Tuple[T_co]:
+        sample = tuple(
+            dataset[torch.randint(high=len(self), size=(1,)).item()]
+            for dataset in self.datasets
+        )
+        return sample
+
+    def __len__(self) -> int:
+        return self.dataset_len
+
+
+def make_dataset(
+    directory: str,
+    extensions: Optional[Tuple[str, ...]] = None,
+    is_valid_file: Optional[Callable[[str], bool]] = None,
+) -> List[str]:
+    directory = os.path.expanduser(directory)
+
+    both_none = extensions is None and is_valid_file is None
+    both_something = extensions is not None and is_valid_file is not None
+    if both_none or both_something:
+        raise ValueError(
+            "Both extensions and is_valid_file cannot be None or not None at the same time"
+        )
+
+    if extensions is not None:
+
+        def is_valid_file(x: str) -> bool:
+            return has_file_allowed_extension(x, cast(Tuple[str, ...], extensions))
+
+    is_valid_file = cast(Callable[[str], bool], is_valid_file)
+
+    instances = []
+    for root, _, fnames in sorted(os.walk(directory, followlinks=True)):
+        for fname in sorted(fnames):
+            path = os.path.join(root, fname)
+            if is_valid_file(path):
+                instances.append(path)
+
+    return instances
+
+
+class ImageDataset(Dataset[torch.Tensor]):
+    def __init__(
+        self,
+        root: str,
+        transform: Optional[Callable] = None,
+        loader: Callable[[str], Any] = default_loader,
+        is_valid_file: Optional[Callable[[str], bool]] = None,
+    ) -> None:
+        super().__init__()
+        self.root = root
+        self.transform = transform
+        self.loader = loader
+
+        self.samples = make_dataset(
+            root,
+            IMG_EXTENSIONS if is_valid_file is None else None,
+            is_valid_file=is_valid_file,
+        )
+
+    def __getitem__(self, index: int) -> torch.Tensor:
+        path = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        return sample
+
+    def __len__(self) -> int:
+        return len(self.samples)
